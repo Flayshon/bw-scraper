2ff7148db7f9b45e349b784c699534b4f93ea780
flayshon <flayshon@gmail.com>
2018-07-26 22:40:08 -0300

    Refining the parsing method

--- scraper.py
+++ scraper.py
@@ -16,24 +16,29 @@ def pageScraper(url):
     out = open(rawFileName, 'w')
 
     for q in questions:
-        date = re.findall(r'<dco>(.*?)</dco>', q, re.DOTALL)
-        if date:
-            date, time = date[0].split('&nbsp; \n')
-            out.write('[' + date + ' - ' + time + ']' + '\n')
+        date, question = q.split('</dco>')
+        #print(date)
+        date, time = date.split('&nbsp; \\n')
+        out.write('[' + date + ' - ' + time + ']' + '\n')
+        soup = BeautifulSoup(question, "html.parser")
+        print(soup.get_text())
 
-            qnaList = re.findall(r'<qco>(.*?)<h3>', str(q), re.DOTALL)
-            qnaList = [item + '\n' for item in qnaList]
-            for q in qnaList:
-                soup = BeautifulSoup(q, "html.parser")
-                out.write(soup.get_text())
-
-            lastSubQuestion = regex.findall(r'(?r)<qco>(.*?)', str(q), re.DOTALL)
-            soup = BeautifulSoup(lastSubQuestion[0], "html.parser")
+        #qnaList = [item + '\n' for item in qnaList]
+        question = question.split('<\qco>')
+        for sub in question:
+            sub = sub.strip('<qco>')
+            soup = BeautifulSoup(sub, "html.parser")
             out.write(soup.get_text() + '\n\n')
 
     #Last entry always falls in a different pattern, so parsing right-to-left is faster.
-    lastQuestion = regex.findall(r'(?r)<h3>(.*?)<A NAME="bottom">', str(respData))
-    out.write(lastQuestion[0] + '\n')
+    lastQ = regex.findall(r'(?r)<h3> <dco>(.*?)<A NAME="bottom">', str(respData))
+    date, lastQuestion = lastQ[0].split('</dco>')
+    date, time = date.split('&nbsp; \\n')
+    out.write('[' + date + ' - ' + time + ']' + '\n')
+    question = question.split('<\qco>')
+    for sub in question:
+        soup = BeautifulSoup(sub, "html.parser")
+        out.write(soup.get_text() + '\n\n')
 
     nextUrlData = regex.findall(r'(?r)<a href="questions-20(.*?).html">', str(respData))
     
@@ -48,26 +53,6 @@ def pageScraper(url):
     out.close()
     
     return nextUrl
-
-def stripTags(rawFileName, cleanFileName):
-    print('Cleaning the file...')
-    with open(rawFileName, 'r') as raw, open(cleanFileName, 'a') as clean:
-        for question in raw:
-            date = re.findall(r'<dco>(.*?)</dco>', str(question), re.DOTALL)
-            if date:
-                print(date[0])
-                date, time = date[0].split('&nbsp; \n')
-                clean.write('[' + date + ' - ' + time + ']' + '\n')
-
-                qnaList = re.findall(r'<qco>(.*?)<h3>', str(question), re.DOTALL)
-                qnaList = [item + '\n' for item in qnaList]
-                for q in qnaList:
-                    soup = BeautifulSoup(q, "html.parser")
-                    clean.write(soup.get_text())
-
-                lastSubQuestion = regex.findall(r'(?r)<qco>(.*?)', str(question), re.DOTALL)
-                soup = BeautifulSoup(lastSubQuestion[0], "html.parser")
-                clean.write(soup.get_text() + '\n')
 
 nextUrl = pageScraper(homeUrl)
 '''
